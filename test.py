6e1c640a3ad3da5ca9fbf474442697360bf24f04
JaMusial <maly1328@gmail.com>
2023-12-20 22:28:57 +0100

    Poczatkowa_logika

--- test.py
+++ test.py
@@ -1,6 +1,51 @@
 import time
 import tkinter as tk
 from tkinter import font
+from typing import NamedTuple
+from itertools import cycle
+
+class Player(NamedTuple):
+    label: str     #O lub X
+    colour: str     
+
+class Move(NamedTuple):
+    row: int
+    col: int
+    label: str = ""   #Pamieta znak danego gracza
+
+BOARD_SIZE=3
+DEFAULT_PLAYERS = (
+    Player(label="X", colour="blue"),
+    Player(label="O", colour="green"),
+)
+class TicTacToeGame:
+    def __init__(self, player=DEFAULT_PLAYERS, board_size=BOARD_SIZE):
+        self._players = cycle(players)
+        self.board_size = board_size
+        self.current_player = next(self._players)
+        self.winner_combo = []
+        self._current_moves = []
+        self._has_winner = False
+        self._winning_combos = []
+        self._setup_board()
+
+    def _get_winnig_combos(self):
+        rows = [
+            [(move.row, move.col) for move in row]
+            for row in self._current_moves
+        ]
+        columns = [list(col) for col in zip(*rows)]
+        first_diagonal = [row[i] for i, row in enumerate(rows)]
+        second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
+        return rows + columns + [first_diagonal, second_diagonal]
+
+    def _setup_board(self):
+        self._current_moves = [
+            [Move(row,col) for col in range(self.board_size)]
+            for row in range(self.board_size)
+        ]
+        self._winning_combos = self._get_winnig_combos()
+
 
 class Board(tk.Tk):
     def __init__(self):
